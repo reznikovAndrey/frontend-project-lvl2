2ee661045457019dab400c83b72779fa0de269d8
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-06-07 17:13:42 +0500

    Add `stylish` format as default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .version('1.0.0')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .action((filepath1, filepath2, format) => console.log(genDiff(filepath1, filepath2, format)))
   .parse(process.argv);
