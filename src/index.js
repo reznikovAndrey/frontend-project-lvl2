73f67ca88ebb14ba9963b5492618420896ffd337
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-06-07 16:38:48 +0500

    Refactor `index.js`
    - Import needed function from lodash
    - Add ability to work with nested data

--- src/index.js
+++ src/index.js
@@ -1,52 +1,35 @@
-import _ from 'lodash';
+import sortBy from 'lodash/sortBy.js';
+import isObject from 'lodash/isObject.js';
 
 import getParsedData from './parsers.js';
 
-const getDiff = (filepath1, filepath2) => {
+const checkChildren = (val1, val2) => (
+  (isObject(val1) && !Array.isArray(val1)) && (isObject(val2) && !Array.isArray(val2))
+);
+
+export default (filepath1, filepath2) => {
   const fileObj1 = getParsedData(filepath1);
   const fileObj2 = getParsedData(filepath2);
 
-  const keys = _.sortBy(Object.keys({ ...fileObj1, ...fileObj2 }));
-
-  const result = keys.map((key) => {
-    const val1 = fileObj1[key];
-    const val2 = fileObj2[key];
-
-    if (!_.has(fileObj1, key)) {
-      return { type: 'added', key, val: val2 };
-    }
-
-    if (!_.has(fileObj2, key)) {
-      return { type: 'removed', key, val: val1 };
-    }
-
-    if (val1 !== val2) {
-      return { type: 'changed', key, val: [val1, val2] };
-    }
-
-    return { type: 'unchanged', key, val: val1 };
-  });
-
-  const outputResult = result.map(({ type, key, val }) => {
-    switch (type) {
-      case 'added':
-        return `  + ${key}: ${val}`;
-      case 'removed':
-        return `  - ${key}: ${val}`;
-      case 'changed': {
-        const [val1, val2] = val;
-        const str1 = `  - ${key}: ${val1}`;
-        const str2 = `  + ${key}: ${val2}`;
-        return [str1, str2].join('\n');
+  const iter = (obj1, obj2) => {
+    const keys = sortBy(Object.keys({ ...fileObj1, ...fileObj2 }));
+    return keys.map((key) => {
+      if (!Object.hasOwn(obj1, key)) {
+        return { key, type: 'added', value: obj2[key] };
       }
-      case 'unchanged':
-        return `    ${key}: ${val}`;
-      default:
-        return '';
-    }
-  });
+      if (!Object.hasOwn(obj2, key)) {
+        return { key, type: 'removed', value: obj1[key] };
+      }
+      const isChildren = checkChildren(obj1[key], obj2[key]);
+      if (isChildren) {
+        return { key, type: 'nested', children: iter(obj1[key], obj2[key]) };
+      }
+      return obj1[key] === obj2[key]
+        ? { key, type: 'unchanged', value: obj1[key] }
+        : { key, type: 'changed', valuesObj: { old: obj1[key], new: obj2[key] } };
+    });
+  };
 
-  return `{\n${outputResult.join('\n')}\n}`;
+  const nodes = iter(fileObj1, fileObj2);
+  return nodes;
 };
-
-export default getDiff;
